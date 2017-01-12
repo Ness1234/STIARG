094a3a2b0e5fdf3c86f18251d758158b3633fb74
Torben Lehmann <torben.leh@outlook.com>
2017-01-12 20:40:46 +0100

    Added 'myCar'
    Transforms the car out of the single dot. Doesn't draw it tho

--- Main.hs
+++ Main.hs
@@ -7,10 +7,12 @@ import qualified Network.Socket as Sock
 
 
 move :: (Int, Int) -> String -> (Int, Int) -> (Int, Int)
-move (xdim, ydim) "\"s\"" (x, y) = if (y /= 11) then (x, (y + 4)) else (x, y)
-move (xdim, ydim) "\"w\"" (x, y) = if (y /= 3) then (x, (y - 4)) else (x, y)
+move (xdim, ydim) "\"s\"" (x, y) = if (y /= 10) then (x, (y + 4)) else (x, y)
+move (xdim, ydim) "\"w\"" (x, y) = if (y /= 2) then (x, (y - 4)) else (x, y)
 move _ _ x = x
 
+myCar :: (Int, Int) -> [(Int, Int)]
+myCar (x, y) = [(x, y+1), (x, y-1), (x + 1, y), (x + 2, y), (x + 3, y + 1), (x + 3, y - 1), (x + 3, y), (x + 4, y)]
 
 toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
 toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xc0 0xcb else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
@@ -22,4 +24,4 @@ eventTest events pixel = (toFrame dim pixel', pixel')
 dim :: (Int, Int)
 dim = (30, 12)
 main :: IO ()
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest (1, 7)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest [(1, 6)]
