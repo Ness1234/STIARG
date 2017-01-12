55bfb162bac4be55a1dd27393681a5bead3dd8ea
Torben Lehmann <torben.leh@outlook.com>
2017-01-12 17:26:39 +0100

    Added Borders; Point now Pink; Point moved 1px down

--- Main.hs
+++ Main.hs
@@ -7,15 +7,13 @@ import qualified Network.Socket as Sock
 
 
 move :: (Int, Int) -> String -> (Int, Int) -> (Int, Int)
-move (xdim, ydim) "\"s\"" (x, y) = (x, (y + 4)) -- Add border so you can't go outside the road
-move (xdim, ydim) "\"w\"" (x, y) = (x, (y - 4))
---move (xdim, ydim) "\"a\"" (x, y) = ((x - 1) `mod` xdim, y)
---move (xdim, ydim) "\"d\"" (x, y) = ((x + 1) `mod` xdim, y)
+move (xdim, ydim) "\"s\"" (x, y) = if (y /= 11) then (x, (y + 4)) else (x, y)
+move (xdim, ydim) "\"w\"" (x, y) = if (y /= 3) then (x, (y - 4)) else (x, y)
 move _ _ x = x
 
 
 toFrame :: (Int, Int) -> (Int, Int) -> ListFrame
-toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xff 0xff else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
+toFrame (xdim, ydim) (x', y') = ListFrame $ map (\y -> map (\x -> if x == x' && y == y' then Pixel 0xff 0xc0 0xcb else Pixel 0 0 0) [0 .. xdim - 1]) [0 .. ydim - 1]
 
 eventTest :: [Event String] -> (Int, Int) -> (ListFrame, (Int, Int))
 eventTest events pixel = (toFrame dim pixel', pixel')
@@ -24,4 +22,4 @@ eventTest events pixel = (toFrame dim pixel', pixel')
 dim :: (Int, Int)
 dim = (30, 12)
 main :: IO ()
-main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest (1, 6)
+main = Sock.withSocketsDo $ runMate (Config (fromJust $ parseAddress "134.28.70.172") 1337 dim (Just 500000) False []) eventTest (1, 7)
